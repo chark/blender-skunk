1ae3f2bc77759e39e7ed9a4c6d222ba45fe91d72
Edvinas Danevičius <edvinas108@gmail.com>
2025-12-04 19:15:03 +0200

    Fixup gh script

--- .github/workflows/create-github-release.sh
+++ .github/workflows/create-github-release.sh
@@ -6,9 +6,13 @@ set -euo pipefail
 releaseVersion="${RELEASE_VERSION}"
 releaseMessage="${RELEASE_MESSAGE}"
 
+# Find package
+echo 'Looking for release .zip file'
+zipFile=$(find . -maxdepth 1 -type f -name '*.zip' | head -n 1)
+
 # Execute
 echo "Creating GitHub release ${releaseVersion}"
-gh release create releaseVersion \
-  zipfile \
+gh release create "${releaseVersion}" \
+  "${zipFile}" \
   --title "${releaseVersion}" \
   --notes "$(cat "${releaseMessage}")"
