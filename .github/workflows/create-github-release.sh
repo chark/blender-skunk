fc5eb509f7f18102d906a4be6174e9ffb2af70c2
Edvinas Danevičius <edvinas108@gmail.com>
2025-12-04 19:15:54 +0200

    Better handling for release notes

--- .github/workflows/create-github-release.sh
+++ .github/workflows/create-github-release.sh
@@ -6,6 +6,14 @@ set -euo pipefail
 releaseVersion="${RELEASE_VERSION}"
 releaseMessage="${RELEASE_MESSAGE}"
 
+# Create notes
+echo 'Creating release notes'
+if [[ -f "${releaseMessage}" ]]; then
+  notes=$(cat "${releaseMessage}")
+else
+  notes="${releaseMessage}"
+fi
+
 # Find package
 echo 'Looking for release .zip file'
 zipFile=$(find . -maxdepth 1 -type f -name '*.zip' | head -n 1)
@@ -15,4 +23,4 @@ echo "Creating GitHub release ${releaseVersion}"
 gh release create "${releaseVersion}" \
   "${zipFile}" \
   --title "${releaseVersion}" \
-  --notes "$(cat "${releaseMessage}")"
+  --notes "${notes}"
